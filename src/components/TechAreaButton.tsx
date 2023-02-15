1e91b38d53271a21fd2b8cd8b82ce1cb1b19515f
felipemalli <felipevahia@gmail.com>
2023-02-15 20:10:49 -0300

    feat: add techAreaButton component

---
+++ src/components/TechAreaButton.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export const TechAreaButton: React.FC<{techArea: string, enable: boolean}> = ({ techArea, enable }: {techArea: string, enable: boolean}) => {
+
+  const [screen, setScreen] = useState<'photoFrame' | 'singleSkillTable' | 'doubleSkillTable'>('photoFrame');
+
+  return (
+    <>
+      {
+        enable ? (
+          <button className="w-24 h-8 bg-[#d9d9d9] hover:bg-[#dedede] rounded-md text-sm">
+            {techArea}
+          </button>
+        ) : (
+          <></>
+          // <button className="w-16 h-8 bg-[#d9d9d9] hover:bg-[#dedede] rounded-md text-sm opacity-40">
+          //   {techArea}
+          // </button>
+        )
+      }
+    </>
+  )
+}
