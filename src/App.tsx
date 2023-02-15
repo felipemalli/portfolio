fc38d5714371489c1b39840f5a810986739f14d3
felipemalli <felipevahia@gmail.com>
2023-02-15 04:24:28 -0300

    feat: create card section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import CardSection from './components/CardSection'
 
 function App() {
   const [count, setCount] = useState(0)
   
   return (
-    <div className="bg-yellow-600">
+    <div className="bg-gray-100">
       <p className="text-gray-700">
         Portfolio
       </p>
+      <CardSection></CardSection>
     </div>
   )
 }
---
+++ src/components/CardSection.tsx
@@ -0,0 +1,13 @@
+import ProjectCard from './ProjectCard'
+
+export default function CardSection() {
+  
+  return (
+    <section className='flex justify-center '>
+      <ProjectCard></ProjectCard>
+      <ProjectCard></ProjectCard>
+      <ProjectCard></ProjectCard>
+      <ProjectCard></ProjectCard>
+    </section>
+  )
+}
