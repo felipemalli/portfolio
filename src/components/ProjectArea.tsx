5042b4cb233d2dbbaffbac1ecb93151fc81fe75b
felipemalli <felipevahia@gmail.com>
2023-02-15 20:25:53 -0300

    feat: add titles for project and course areas

--- src/components/ProjectArea.tsx
+++ src/components/ProjectArea.tsx
@@ -1,13 +1,17 @@
 import { Card } from './Card'
 
 export default function ProjectArea() {
-  
+
   return (
-    <div className='flex justify-center items-center gap-[0.938rem] bg-[#D5685A] w-[89.938rem] h-[26rem] m-auto'>
-      <Card type='project' name='Portfolio'></Card>
-      <Card type='project' name='Trybe Futebol Club'></Card>
-      <Card type='project' name='Drone Feeder'></Card>
-      <Card type='project' name='Clean Schedule'></Card>
+    <div className='flex flex-col justify-evenly items-center bg-[#D5685A] w-[89.938rem] h-[26rem] m-auto rounded-t-[5rem]'>
+      <h2 className='text-4xl font-bold text-[#f9f9f9]'>Projects</h2>
+      <div className='flex justify-center items-center gap-[0.938rem]'>
+        <Card type='project' name='Portfolio'></Card>
+        <Card type='project' name='Trybe Futebol Club'></Card>
+        <Card type='project' name='Drone Feeder'></Card>
+        <Card type='project' name='Clean Schedule'></Card>
+        <Card type='project' name='Clean Schedule'></Card>
+      </div>
     </div>
   )
 }
