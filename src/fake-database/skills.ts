18e19ad18d1cea0f97fab3d524aed1e70b38f087
felipemalli <felipevahia@gmail.com>
2023-02-16 03:06:42 -0300

    feat: add skills to fake database

---
+++ src/fake-database/skills.ts
@@ -0,0 +1,10 @@
+const devopsSkills: string[] = ['Docker', 'Kubernetes', 'AWS', 'Cloud']
+
+const backendSkills: string[] = ['Javascript', 'Typescript', 'Node', 'Express', 'Sequelize', 'Prisma', 'Nest', 'Jest', 'MySQL', 'MongoDB', 'Swagger', 'Java', 'Spring', 'Hibernate', 'Quarkus', 'JUnit', 'Python', 'JWT', 'Cookies', 'BCrypt']
+
+const frontendSkills: string[] = ['HTML', 'CSS', 'Javascript', 'Typescript', 'React', 'Redux', 'Next', 'Jest', 'Tailwind', 'Material-UI', 'Figma']
+
+const otherSkills: string[] = ['Design Patterns, OOP, Microservices, Clean Architecture']
+
+
+export { frontendSkills, backendSkills, devopsSkills, otherSkills }
