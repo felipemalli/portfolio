4824f3802edd24e9bc3925ed9787841fd465274f
felipemalli <felipevahia@gmail.com>
2023-02-16 20:54:38 -0300

    style: change colors and add border to course section

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
   
   return (
-    <div className="bg-[#fbfbfb] h-screen">
+    <div className="bg-[#fbfbfb] h-auto">
       <header></header>
       <main>
         <p className="text-gray-700">
