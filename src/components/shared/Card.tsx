483658b095ebb4701cfaf5f247358c2d8459ab82
felipemalli <felipevahia@gmail.com>
2023-02-20 03:35:02 -0300

    refactor: change card and tech area button components to other folder strucutre

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,5 +1,3 @@
-import { motion } from 'framer-motion';
-import { useState } from 'react';
 import { TechAreaButton } from './TechAreaButton';
 
 export interface CardProps {
@@ -9,12 +7,13 @@ export interface CardProps {
   image: string,
 }
 
+
 export const Card: React.FC<CardProps> = ({ cardKey, type, name, image }: CardProps) => {
 
-  const [screen, setScreen] = useState<'photoFrame' | 'singleSkillTable' | 'doubleSkillTable'>('photoFrame');
+  // const [screen, setScreen] = useState<'photoFrame' | 'singleSkillTable' | 'doubleSkillTable'>('photoFrame');
 
   return (
-    <motion.div whileHover={{ scale: 1.05 }} key={cardKey} className={`flex flex-col justify-evenly	w-56 p- content-between items-center border rounded-2xl border-gray-300 shadow-md ${type === 'project' ? 'h-[14.3rem] bg-[#f9f9f9]' : 'h-[14.75rem] bg-[#f7f7f7]'}`}>
+    <div key={cardKey} className={`mx-[7.5px] hover:scale-105 flex flex-col justify-evenly	w-56 p- content-between items-center border rounded-2xl border-gray-300 shadow-md ${type === 'project' ? 'h-[14.3rem] bg-[#f9f9f9]' : 'h-[14.75rem] bg-[#f7f7f7]'}`}>
       <h3 className={`pointer-events-auto cursor-pointer flex items-center justify-center h-10 w-56  pt-1 text-gray-900 font-medium text-center
       ${type === 'project' ? 'text-lg' : 'text-sm'}`}
         onClick={ () => console.log('teste')}
@@ -23,13 +22,13 @@ export const Card: React.FC<CardProps> = ({ cardKey, type, name, image }: CardPr
       </h3>
       <hr className='w-52 h-0.5 bg-[#d9d9d9] border-0 rounded m-0 p-0'></hr>
       <button onClick={ () => console.log('') } className='flex items-center justify-center w-52 h-32 bg-[#d9d9d9] rounded-md'>
-        <img src={image} alt='Course image' className='bg-cover flex h-32 w-52 border rounded-lg'></img>
+        <img src={image} alt='Course image' className='bg-cover flex h-32 w-52 border rounded-lg'/>
       </button>
       <div className='flex space-x-2'>
         <TechAreaButton techArea='Front' enable={true}></TechAreaButton>
         <TechAreaButton techArea='Back' enable={true}></TechAreaButton>
         <TechAreaButton techArea='Devops' enable={true}></TechAreaButton>
       </div>
-    </motion.div>
+    </div>
   )
 }
