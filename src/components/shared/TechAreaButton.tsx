497b4f624988af720b05ee0036038c9907408200
felipemalli <felipevahia@gmail.com>
2023-02-21 08:36:48 -0300

    feat: implement logic to travel between frames at each card

--- src/components/shared/TechAreaButton.tsx
+++ src/components/shared/TechAreaButton.tsx
@@ -1,21 +1,22 @@
-// import { useState } from 'react';
+import { FrameState, TechArea } from './Card';
 
-export const TechAreaButton: React.FC<{techArea: string, enable: boolean}> = ({ techArea, enable }: {techArea: string, enable: boolean}) => {
+interface TechAreaProps {
+  techAreaAbreviation: string,
+  techArea: TechArea,
+  enable: boolean,
+  setFrame: (arg0: FrameState) => void,
+}
 
-  // const [screen, setScreen] = useState<'photoFrame' | 'singleSkillTable' | 'doubleSkillTable'>('photoFrame');
-
+export const TechAreaButton: React.FC<TechAreaProps> = ({ techAreaAbreviation, techArea, enable, setFrame }: TechAreaProps) => {
   return (
     <>
       {
-        enable ? (
-          <button className='w-16 h-8 bg-[#d9d9d9] hover:bg-[#e4e4e4] active:bg-green-400 rounded-md text-sm transition-color duration-200 '>
-            {techArea}
+        enable && (
+          <button className='w-16 h-8 bg-[#d9d9d9] hover:bg-[#e4e4e4] active:bg-green-400 rounded-md text-sm transition-color duration-200'
+            onClick={()=> setFrame({ screen: 'skillFrame', page: 1, techArea: `${techArea}` })}
+          >
+            {techAreaAbreviation}
           </button>
-        ) : (
-          <></>
-          // <button className='w-16 h-8 bg-[#d9d9d9] hover:bg-[#dedede] rounded-md text-sm opacity-40'>
-          //   {techArea}
-          // </button>
         )
       }
     </>
