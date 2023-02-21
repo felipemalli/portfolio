d4c05c233fd993f8f01612f4e3f23c20b5053b8a
felipemalli <felipevahia@gmail.com>
2023-02-21 08:30:30 -0300

    style: ajust position of texts at first screen on mobile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import { NavOption } from './particular/NavOption';
 
 export const Header: React.FC = () => {
   return (
-    <header className='flex justify-center xl:justify-start p-6 my-6 xl:ml-20 2xl:ml-12'>
+    <header className='hidden md:flex justify-center xl:justify-start p-6 my-6 xl:ml-20 2xl:ml-12'>
       <nav className='hidden items-center space-x-8 uppercase text-gray-400 md:flex'>
         <NavOption>Projects</NavOption>
         <NavOption>Courses</NavOption>
