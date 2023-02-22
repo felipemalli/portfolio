d1681057089f656da9b9975669a1295f588ae23b
felipemalli <felipevahia@gmail.com>
2023-02-22 02:15:11 -0300

    feat: change titles and sizes when content card has changed

--- src/components/content-card-section/TopContentCard.tsx
+++ src/components/content-card-section/TopContentCard.tsx
@@ -1,16 +1,21 @@
-import { backendSkills } from '../../data/skills';
+import { useChangeAreaContext } from '../../contexts/changeAreaContext';
+import { topSkills } from '../../data/skills';
 import { CARD_SIZE } from '../shared/Card';
 import { SliderFiveColumns } from '../shared/SliderFiveColumns';
 import { ProjectArea } from './particular/ProjectArea';
 import { SkillArea } from './particular/SkillArea';
 
 export const TopContentCard: React.FC = () => {
+  const { areaName } = useChangeAreaContext();
+
   return (
     <div className='flex flex-col justify-evenly items-center h-[26rem] m-auto md:rounded-t-[5rem] shadow-inner bg-[#D5685A]'>
-      <h2 className='text-4xl font-bold text-[#f9f9f9] select-none -mb-5'>Projects</h2>
+      <h2 className={`text-[#f9f9f9] select-none -mb-5 font-semibold ${areaName === 'knowledge' ? 'text-4xl ' : 'text-[2rem]'}`}>
+        {areaName === 'knowledge' ? 'Projects' : 'BackEnd + DevOps'}
+      </h2>
       <SliderFiveColumns CARD_SIZE={CARD_SIZE}>
         <ProjectArea/>
-        <SkillArea skills={backendSkills}/>
+        <SkillArea skills={topSkills}/>
       </SliderFiveColumns>
     </div>
   );
