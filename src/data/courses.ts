744bbdcf0cb501942097702064c4ea704bf5d921
felipemalli <felipevahia@gmail.com>
2023-02-22 20:54:09 -0300

    refactor: change course and project data models to fit a single interface ICard

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,5 +1,9 @@
-export interface ICourse {
+type ILinkName = 'Certificate' | 'Code' | 'Browser' | 'API'
+type IBottonRightTag = 'Hours' | 'Browser'
+
+export interface ICard {
   name: string,
+  creator: string,
   description: string,
   inProgress: boolean,
   image?: string,
@@ -7,26 +11,30 @@ export interface ICourse {
     frontend?: {
       inProgress?: boolean,
       skills?: string[],
-    }
+    },
     backend?: {
       inProgress?: boolean,
       skills?: string[],
-    }
+    },
     devops?: {
       inProgress?: boolean,
       skills?: string[],
-    }
-  }
-  extras: {
-    hours: number,
-    certificateLink?: string,
-    instructor: string,
-  }
+    },
+  },
+  links: {
+    name: ILinkName,
+    link: string,
+  }[],
+  bottonRightTag: {
+    name: IBottonRightTag,
+    value?: number
+  },
 }
 
-export const courses: ICourse[] = [
+export const courses: ICard[] = [
   {
     name: 'Microservices with NodeJS and React',
+    creator: 'Stephen Grinder',
     description: 'I learned Kubernetes and all the logic of microservices, their various problems and alternatives. The course taught how a message broker works and its integration with the various Microservices. In general, I learned how to build, deploy, and scale an E-Commerce app using Microservices.',
     inProgress: false,
     image: 'src/assets/courses/microservices-stephengrinder.jpg',
@@ -41,14 +49,20 @@ export const courses: ICourse[] = [
         skills: ['Docker', 'Kubernetes', 'Scaffolding'],
       }
     },
-    extras: {
-      hours: 54.5,
-      instructor: 'Stephen Grinder',
-      certificateLink: 'https://www.udemy.com/certificate/UC-09cd33eb-0627-427f-bf86-f36e299f469f/',
-    }
+    links: [
+      {
+        name: 'Certificate',
+        link: 'https://www.udemy.com/certificate/UC-09cd33eb-0627-427f-bf86-f36e299f469f/'
+      },
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 54.5,
+    },
   },
   {
     name: 'Amazon Web Services (AWS): Essential',
+    creator: 'Geek University',
     description: 'I learned all essential tools of AWS. IAM, network and connectivity services, EC2, S3, SQL service (RDS), NoSQL service (DynamoDB), Cloud Trail and CloudWatch, Elastic Load Balancing, Route 53, CloudFront and a quick look at serverless computing (Lambda functions).',
     inProgress: false,
     image: 'src/assets/courses/aws-geekuniversity.jpg',
@@ -57,14 +71,20 @@ export const courses: ICourse[] = [
         skills: ['AWS'],
       }
     },
-    extras: {
-      hours: 12.5,
-      instructor: 'Geek University',
-      certificateLink: 'https://www.udemy.com/certificate/UC-889787a5-7efc-41f7-9ad8-8f336c7fe421/',
-    }
+    links: [
+      {
+        name: 'Certificate',
+        link: 'https://www.udemy.com/certificate/UC-889787a5-7efc-41f7-9ad8-8f336c7fe421/'
+      },
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 12.5,
+    },
   },
   {
     name: 'Full Stack web development',
+    creator: 'Trybe',
     description: 'Where I learned to code. In addition to the thousands of hours invested in learning, I\'ve done a huge amount of projects in this course. Most aren\'t on github.',
     inProgress: false,
     image: 'src/assets/courses/webdevelopment-trybe.jpg',
@@ -79,13 +99,20 @@ export const courses: ICourse[] = [
         skills: ['Docker'],
       }
     },
-    extras: {
-      hours: 1500,
-      instructor: 'Brad Traversy',
-    }
+    links: [
+      {
+        name: 'Certificate',
+        link: '',
+      },
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 1500,
+    },
   },
   {
     name: 'Java Bootcamp',
+    creator: 'Wipro and Trybe',
     description: 'I learned Java for backend. Hibernate, Spring, Quarkus, JUnit, Docker, more about dependecy injections and a lot of other knowledges. I\'ve done a lot of projects in this course and most aren\'t on github. ',
     inProgress: false,
     image: 'src/assets/courses/javabootcamp-trybe-and-wipro.jpg',
@@ -97,13 +124,20 @@ export const courses: ICourse[] = [
         skills: ['Docker'],
       }
     },
-    extras: {
-      hours: 500,
-      instructor: 'Wipro and Trybe',
-    }
+    links: [
+      {
+        name: 'Certificate',
+        link: '',
+      }
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 500,
+    },
   },
   {
     name: 'PRO FIGMA | User interface design from beginner to expert',
+    creator: 'Lucas Marte',
     description: 'I am learning how to use Figma to design my mobile/web pages and a lot of tips about UI/UX.',
     inProgress: true,
     image: 'src/assets/courses/profigma-lucasmarte.jpg',
@@ -112,13 +146,20 @@ export const courses: ICourse[] = [
         skills: ['Figma'],
       }
     },
-    extras: {
-      hours: 14.5,
-      instructor: 'Lucas Marte',
+    links: [
+      {
+        name: 'Certificate',
+        link: '',
+      }
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 14.5,
     }
   },
   {
     name: 'Tailwind CSS from scratch | Learn by building projects',
+    creator: 'Brad Traversy',
     description: 'I am learning Tailwind and how to make excellent projects with this technologies',
     inProgress: true,
     image: 'src/assets/courses/tailwind-bradtraversy.jpg',
@@ -127,13 +168,20 @@ export const courses: ICourse[] = [
         skills: ['HTML', 'Tailwind'],
       }
     },
-    extras: {
-      hours: 12.5,
-      instructor: 'Brad Traversy',
+    links: [
+      {
+        name: 'Certificate',
+        link: '',
+      }
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 12.5,
     }
   },
   {
     name: 'NodeJs, Typescript, TDD, DDD, Clean Architecture e SOLID',
+    creator: 'Rodrigo Manguinho',
     description: 'I am learning ...',
     inProgress: true,
     image: 'src/assets/courses/cleanarchitecture-rodrigomanguinho.jpg',
@@ -145,9 +193,15 @@ export const courses: ICourse[] = [
         skills: [''],
       }
     },
-    extras: {
-      hours: 12.5,
-      instructor: 'Rodrigo Manguinho',
+    links: [
+      {
+        name: 'Certificate',
+        link: '',
+      }
+    ],
+    bottonRightTag: {
+      name: 'Hours',
+      value: 47.5,
     }
   },
 ];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,32 +1,9 @@
-export interface IProject {
-  name: string,
-  description: string,
-  inProgress: boolean,
-  image?: string,
-  techAreas: {
-    frontend?: {
-      inProgress?: boolean,
-      skills?: string[],
-    }
-    backend?: {
-      inProgress?: boolean,
-      skills?: string[],
-    }
-    devops?: {
-      inProgress?: boolean,
-      skills?: string[],
-    }
-  },
-  extras: {
-    browserLink?: string,
-    codeLink: string,
-    apiLink?: string,
-  }
-}
+import { ICard } from './courses';
 
-export const projects: IProject[] = [
+export const projects: ICard[] = [
   {
     name: 'Portfolio',
+    creator: 'Felipe Vahia Malliagros',
     description: 'Project developed to expose my knowledge. Projects, courses taken, skills already trained, my future steps and goals. OBS: I will develop a backend for this application soon!',
     inProgress: true,
     techAreas: {
@@ -34,8 +11,22 @@ export const projects: IProject[] = [
         skills: ['Typescript', 'React', 'Vite', 'Tailwind', 'Figma'],
       }
     },
-    extras: {
-      codeLink: 'https://github.com/felipemalli/portfolio',
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/portfolio'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+    bottonRightTag: {
+      name: 'Browser',
     }
   }
 ];
