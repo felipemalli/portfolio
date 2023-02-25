48cfafadc65179f9e1a1f49c7230e66f706794fe
felipemalli <felipevahia@gmail.com>
2023-02-24 22:48:21 -0300

    feat: course and project updates all and filtered data and use filtered to render

--- src/components/content-card-section/particular/CourseArea.tsx
+++ src/components/content-card-section/particular/CourseArea.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
 import { useChangeAreaContext } from '../../../contexts/changeAreaContext';
+import { useCourseContext } from '../../../contexts/courseContext';
 import { courses } from '../../../data/courses';
+import { ICard } from '../../../interfaces';
 import { Card } from '../../shared/Card';
 
 export const CourseArea: React.FC = () => {
   const { areaName } = useChangeAreaContext();
+  const { setAllCourses, filteredCourses, setFilteredCourses } = useCourseContext();
+
+  useEffect(() => {
+    setAllCourses(courses);
+    setFilteredCourses(courses);
+  }, []);
 
   return (
     <div className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
-      {courses.map((course) => (
+      {filteredCourses && filteredCourses.map((course: ICard) => (
         <Card key={course.name} card={course}></Card>
       ))}
     </div>
--- src/components/content-card-section/particular/ProjectArea.tsx
+++ src/components/content-card-section/particular/ProjectArea.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
 import { useChangeAreaContext } from '../../../contexts/changeAreaContext';
+import { useProjectContext } from '../../../contexts/projectContext';
 import { projects } from '../../../data/projects';
+import { ICard } from '../../../interfaces';
 import { Card } from '../../shared/Card';
 
 export const ProjectArea: React.FC = () => {
   const { areaName } = useChangeAreaContext();
+  const { setAllProjects, filteredProjects, setFilteredProjects } = useProjectContext();
 
+  useEffect(() => {
+    setAllProjects(projects);
+    setFilteredProjects(projects);
+  }, []);
+  
   return (
     <div className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
-      {projects.map((project: ICard) => (
+      {filteredProjects && filteredProjects.map((project: ICard) => (
         <Card key={project.name} card={project}></Card>
       ))}
     </div>
