ae2f044692149d1d350a65ad0f976e64bf68833f
felipemalli <felipevahia@gmail.com>
2023-02-25 20:25:44 -0300

    feat: header and navoption now move to center of passed id and change card content if necessary

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,14 @@
 import { NavOption } from './particular/NavOption';
 
 export const Header: React.FC = () => {
+
   return (
     <header className='hidden md:flex justify-center xl:justify-start p-6 my-6 xl:ml-20 2xl:ml-12'>
       <nav className='hidden items-center space-x-8 uppercase text-gray-400 md:flex'>
-        <NavOption>Projects</NavOption>
-        <NavOption>Courses</NavOption>
-        <NavOption>Skills</NavOption>
-        <NavOption>More about me</NavOption>
+        <NavOption componentId='content' areaNameNav='knowledge'>Knowledge</NavOption>
+        <NavOption componentId='content' areaNameNav='skills'>Skills</NavOption>
+        <NavOption componentId=''>Contact</NavOption>
+        <NavOption componentId=''>More about me</NavOption>
       </nav>
     </header>
   );
--- src/components/header/particular/NavOption.tsx
+++ src/components/header/particular/NavOption.tsx
@@ -1,8 +1,25 @@
+import React from 'react';
+import { IAreaName, useChangeAreaContext } from '../../../contexts';
 import { IChildrenProps } from '../../../interfaces';
+import { moveScreenToCenterOfId } from '../../../utils/moveScreenToCenterOfId';
 
-export const NavOption: React.FC<IChildrenProps> = ({children}: IChildrenProps) => {
+interface NavOptionProps extends IChildrenProps {
+  componentId: string;
+  areaNameNav?: IAreaName;
+}
+
+export const NavOption: React.FC<NavOptionProps> = ({ children, componentId, areaNameNav }: NavOptionProps) => {
+  const { areaName, toggleChangeArea } = useChangeAreaContext();
+  
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    moveScreenToCenterOfId(e, componentId);
+    if (areaNameNav && areaNameNav !== areaName) {
+      toggleChangeArea();
+    }
+  };
+
   return (
-    <a href='#projects' className="text-[#58656F] tracking-widest font-semibold hover:scale-105">
+    <a href={`#${componentId}`} className="text-[#58656F] tracking-widest font-semibold hover:scale-105" onClick={(e) => handleClick(e)}>
       {children}
     </a>
   );
