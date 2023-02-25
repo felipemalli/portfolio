319b350bcf6f5f815f202f2d50e9f05864227fed
felipemalli <felipevahia@gmail.com>
2023-02-25 08:00:49 -0300

    perf: optimize code for show only when have content

--- src/components/content-card-section/particular/CourseArea.tsx
+++ src/components/content-card-section/particular/CourseArea.tsx
@@ -10,13 +10,13 @@ export const CourseArea: React.FC = () => {
   const { setAllCourses, filteredCourses, setFilteredCourses } = useCourseContext();
 
   useEffect(() => {
-    setAllCourses(courses);
     setFilteredCourses(courses);
-  }, []);
+    setAllCourses(courses);
+  }, [setAllCourses, setFilteredCourses]);
 
   return (
     <div className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
-      {filteredCourses && filteredCourses.map((course: ICard) => (
+      {filteredCourses.map((course: ICard) => (
         <Card key={course.name} card={course}></Card>
       ))}
     </div>
--- src/components/content-card-section/particular/ProjectArea.tsx
+++ src/components/content-card-section/particular/ProjectArea.tsx
@@ -10,13 +10,13 @@ export const ProjectArea: React.FC = () => {
   const { setAllProjects, filteredProjects, setFilteredProjects } = useProjectContext();
 
   useEffect(() => {
-    setAllProjects(projects);
     setFilteredProjects(projects);
-  }, []);
-  
+    setAllProjects(projects);
+  }, [setAllProjects, setFilteredProjects]);
+
   return (
     <div className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
-      {filteredProjects && filteredProjects.map((project: ICard) => (
+      {filteredProjects.map((project: ICard) => (
         <Card key={project.name} card={project}></Card>
       ))}
     </div>
