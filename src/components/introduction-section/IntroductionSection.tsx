ff707eed8d369a1542d2e2ce1c6faada68e816ac
felipemalli <felipevahia@gmail.com>
2023-02-25 22:50:50 -0300

    fix: ' was not showing in first description of landing page

--- src/components/introduction-section/IntroductionSection.tsx
+++ src/components/introduction-section/IntroductionSection.tsx
@@ -16,7 +16,7 @@ export const IntroductionSection: React.FC = () => {
           />
           <span> developer</span>
         </div>
-        <p className='text-[#58656F] text-base font-normal text-center xl:text-left mx-7 md:mx-0 mt-7'>I{'\''}m a logic and creativity guy. Specialized in backend, but I like all areas of web development. I{'>'}m always studying to keep myself qualified and updated.</p>
+        <p className='text-[#58656F] text-base font-normal text-center xl:text-left mx-7 md:mx-0 mt-7'>I{'\''}m a logic and creativity guy. Specialized in backend, but I like all areas of web development. I{'\''}m always studying to keep myself qualified and updated.</p>
         <div className='mt-7 text-center xl:text-left'>
           <button className='w-32 h-14 mr-4 bg-[#D5685A] hover:bg-[#e4e4e4] hover:text-[#58656F] text-[#fbfbfb] active:bg-green-400 rounded-md text-sm transition-color duration-200 font-medium'>
             Contact me
