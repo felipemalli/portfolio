cd4fc2006909d86b53c38d0f2039e4f61d17fb40
felipemalli <felipevahia@gmail.com>
2023-02-26 05:45:49 -0300

    feat: use nav components in footer and header

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,14 +1,15 @@
-import { NavOption } from '../shared/NavOption';
+import { NavOptionLink } from '../shared/NavOptionLink';
+import { NavOptionNavigation } from '../shared/NavOptionNavigation';
 
 export const Footer: React.FC = () => {
   return (
     <footer className='flex justify-center p-3 mt-96 bg-[#D46253] opacity-80'>
       <p className='hidden lg:flex lg:absolute lg:left-10 font-medium text-sm text-[#f9f9f9]'>© 2023 Felipe Vahia Malliagros</p>
       <nav className='items-center space-x-4 md:space-x-48 uppercase text-[#f9f9f9] font-medium text-sm'>
-        <NavOption className='hover:opacity-80' componentId='content' areaNameNav='knowledge'>Knowledge</NavOption>
-        <NavOption className='hover:opacity-80' componentId='content' areaNameNav='skills'>Skills</NavOption>
-        <NavOption className='hover:opacity-80' componentId=''>Contact</NavOption>
-        <NavOption className='hover:opacity-80' componentId=''>About me</NavOption>
+        <NavOptionNavigation className='hover:opacity-80' componentId='content' areaNameNav='knowledge'>Knowledge</NavOptionNavigation>
+        <NavOptionNavigation className='hover:opacity-80' componentId='content' areaNameNav='skills'>Skills</NavOptionNavigation>
+        <NavOptionLink className='hover:opacity-80' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>Contact</NavOptionLink>
+        <NavOptionNavigation className='hover:opacity-80' componentId=''>About</NavOptionNavigation>
       </nav>
     </footer>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,14 @@
-import { NavOption } from '../shared/NavOption';
+import { NavOptionLink } from '../shared/NavOptionLink';
+import { NavOptionNavigation } from '../shared/NavOptionNavigation';
 
 export const Header: React.FC = () => {
-
   return (
     <header className='hidden md:flex justify-center xl:justify-start p-6 my-6 xl:ml-20 2xl:ml-12'>
       <nav className='hidden items-center space-x-8 uppercase text-[#58656F] tracking-widest md:flex font-semibold'>
-        <NavOption className='hover:scale-105' componentId='content' areaNameNav='knowledge'>Knowledge</NavOption>
-        <NavOption className='hover:scale-105' componentId='content' areaNameNav='skills'>Skills</NavOption>
-        <NavOption className='hover:scale-105' componentId=''>Contact</NavOption>
-        <NavOption className='hover:scale-105' componentId=''>More about me</NavOption>
+        <NavOptionNavigation className='hover:scale-105' componentId='content' areaNameNav='knowledge'>Knowledge</NavOptionNavigation>
+        <NavOptionNavigation className='hover:scale-105' componentId='content' areaNameNav='skills'>Skills</NavOptionNavigation>
+        <NavOptionNavigation className='hover:scale-105' componentId=''>About</NavOptionNavigation>
+        <NavOptionLink className='hover:scale-105' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>Contact</NavOptionLink>
       </nav>
     </header>
   );
