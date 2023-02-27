53b2e74ab3465361b339cfad65d9e8474fec7ba3
felipemalli <felipevahia@gmail.com>
2023-02-27 18:52:46 -0300

    feat: include article semantic to landing page with new collor for circle

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,19 +7,20 @@ import { IntroductionSection } from '../components/introduction-section/Introduc
 export const LandingPage: React.FC = () => {
   return (
     <div>
-      <div className='w-fill md:mx-16 2xl:w-[89.938rem] 2xl:m-auto'>
-        <motion.div initial={{ y: -100 }} animate={{ y: 0 }} transition={{ type: 'tween', duration: 1.5 }}>
-          <Header />
-        </motion.div>
-        <main>
+      <main>
+        <article className='w-fill md:mx-16 2xl:w-[89.938rem] 2xl:m-auto'>
+          <motion.div initial={{ y: -100 }} animate={{ y: 0 }} transition={{ type: 'tween', duration: 1.5 }}>
+            <Header />
+          </motion.div>
           <motion.div initial={{ opacity: 0.5, y: 700 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'tween', duration: 1.5 }}>
             <IntroductionSection />
             <ContentCardSection />
           </motion.div>
-          <img src="src/assets/background/circle.svg" className='absolute opacity-80 -top-[70%] left-[70%] 2xl:-top-[80%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img>
-        </main>
-      </div>
-      <Footer />
+          <img src="src/assets/background/circle.svg" className='absolute -top-[70%] left-[65%] 2xl:-top-[110%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img>
+          {/* <img src="src/assets/background/circle.svg" className='absolute opacity-80 -top-[70%] left-[70%] 2xl:-top-[80%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img> */}
+        </article>
+        <Footer />
+      </main>
     </div>
   );
 };
