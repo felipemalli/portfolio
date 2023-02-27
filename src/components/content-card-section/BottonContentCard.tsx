9a5b2e94b9451e30a42d9018d24a33f009300dbe
felipemalli <felipevahia@gmail.com>
2023-02-27 02:31:19 -0300

    feat: add to skill area necessary properties and structure for future filtering

--- src/components/content-card-section/BottonContentCard.tsx
+++ src/components/content-card-section/BottonContentCard.tsx
@@ -1,3 +1,4 @@
+import { useSkillContext } from '../../contexts';
 import { useChangeAreaContext } from '../../contexts/ChangeAreaContext';
 import { bottonSkills } from '../../data/skills';
 import { CARD_SIZE } from '../shared/Card';
@@ -7,12 +8,13 @@ import { SkillArea } from './particular/SkillArea';
 
 export const BottonContentCard: React.FC = () => {
   const { areaName } = useChangeAreaContext();
-
+  const { setFilteredBottonSkills, setAllBottonSkills, filteredBottonSkills } = useSkillContext();
+  
   return (
     <div className='flex flex-col justify-evenly items-center h-[22rem] xl:h-[24rem] 2xl:h-[26rem] m-auto md:rounded-b-[5rem] border-y md:border border-[#737E86] bg-[#fbfbfb] pt-1'>
       <SliderFiveColumns CARD_SIZE={CARD_SIZE}>
         <CourseArea/>
-        <SkillArea skills={bottonSkills}/>
+        <SkillArea type={'botton'} skills={bottonSkills} setFilteredSkills={setFilteredBottonSkills} setAllSkills={setAllBottonSkills} filteredSkills={filteredBottonSkills} />
       </SliderFiveColumns>
       <h2 className={`text-[#272727] select-none -mt-5 md:-mt-2 h-10 font-semibold ${areaName === 'knowledge' ? 'text-2xl md:text-4xl ' : 'text-[1.3rem] md:text-[2rem]'}`}>
         {areaName === 'knowledge' ? 'Courses' : 'FrontEnd + UI/UX'}
--- src/components/content-card-section/particular/SkillArea.tsx
+++ src/components/content-card-section/particular/SkillArea.tsx
@@ -1,21 +1,31 @@
 import { motion } from 'framer-motion';
-import { useChangeAreaContext } from '../../../contexts/changeAreaContext';
+import { useEffect } from 'react';
+import { useChangeAreaContext } from '../../../contexts/ChangeAreaContext';
 import { menuVariants } from '../../../utils/motionVariants';
 import { SkillCard } from '../../shared/SkillCard';
 
 interface ISkillAreaProps {
-  skills: string[]
+  type: 'top' | 'botton';
+  skills: string[];
+  setFilteredSkills: (value: string[]) => void,
+  setAllSkills: (value: string[]) => void,
+  filteredSkills: string[];
 }
 
-export const SkillArea: React.FC<ISkillAreaProps> = ({ skills }: ISkillAreaProps) => {
+export const SkillArea: React.FC<ISkillAreaProps> = ({ type, skills, setFilteredSkills, setAllSkills, filteredSkills }: ISkillAreaProps) => {
   const { areaName } = useChangeAreaContext();
+
+  useEffect(() => {
+    setFilteredSkills(skills);
+    setAllSkills(skills);
+  }, []);
 
   return (
     <motion.div animate={areaName === 'skills' ? 'open' : 'closed'} variants={menuVariants} className={`${areaName !== 'skills' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar grid grid-rows-4 grid-flow-col items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
-      { skills.map((name) => (
-        <SkillCard key={name} type={'backend'} name={name}/>
-      )) }
-      {skills.length === 0 && <h3 className='break-normal absolute left-1/2 -translate-x-1/2 font-semibold text-base sm:text-2xl opacity-50'>Skill not found 😭</h3>}
+      {filteredSkills.map((skill) => (
+        <SkillCard key={skill} type={type} name={skill}/> 
+      ))}
+      {filteredSkills.length === 0 && <h3 className='break-normal absolute left-1/2 -translate-x-1/2 font-semibold text-base sm:text-2xl opacity-50'>Skill not found 😭</h3>}
     </motion.div>
   );
 };
