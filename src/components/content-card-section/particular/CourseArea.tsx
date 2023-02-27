5c35e64466350301560e48b1dd2358e055afe55e
felipemalli <felipevahia@gmail.com>
2023-02-27 02:27:56 -0300

    perf: only "fetch" initial data of courses and projects one time

--- src/components/content-card-section/particular/CourseArea.tsx
+++ src/components/content-card-section/particular/CourseArea.tsx
@@ -14,12 +14,12 @@ export const CourseArea: React.FC = () => {
   useEffect(() => {
     setFilteredCourses(courses);
     setAllCourses(courses);
-  }, [setAllCourses, setFilteredCourses]);
+  }, []);
 
   return (
     <motion.div animate={areaName === 'knowledge' ? 'open' : 'closed'} variants={menuVariants} className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
       {filteredCourses.map((course: ICard) => (
-        <Card key={course.name} card={course}></Card>
+        <Card key={course.name} card={course}/>
       ))}
       {filteredCourses.length === 0 && <h3 className='break-normal absolute left-1/2 -translate-x-1/2 font-semibold text-base sm:text-2xl opacity-50'>Courses not found 😭</h3>}
     </motion.div>
--- src/components/content-card-section/particular/ProjectArea.tsx
+++ src/components/content-card-section/particular/ProjectArea.tsx
@@ -14,12 +14,12 @@ export const ProjectArea: React.FC = () => {
   useEffect(() => {
     setFilteredProjects(projects);
     setAllProjects(projects);
-  }, [setAllProjects, setFilteredProjects]);
+  }, []);
 
   return (
     <motion.div animate={areaName === 'knowledge' ? 'open' : 'closed'} variants={menuVariants} className={`${areaName !== 'knowledge' && 'hidden'} w-[242px] md:w-[484px] lg:w-[726px] xl:w-[968px] 2xl:w-[1210px] hide-horizontal-scrollbar flex items-center justify-start overflow-x-auto scroll-smooth h-[16rem]`}>
       {filteredProjects.map((project: ICard) => (
-        <Card key={project.name} card={project}></Card>
+        <Card key={project.name} card={project}/>
       ))}
       {filteredProjects.length === 0 && <h3 className='break-normal absolute left-1/2 -translate-x-1/2 font-semibold text-base sm:text-2xl opacity-50'>Projects not found 😭</h3>}
     </motion.div>
