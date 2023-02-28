24c60deefb9f12c3293af333661b28d73f3dc2dc
felipemalli <felipevahia@gmail.com>
2023-02-27 21:26:52 -0300

    feat: implement about section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { NavOptionNavigation } from '../shared/Nav/NavOptionNavigation';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className='flex justify-center p-3 mt-96 bg-[#D46253] opacity-80'>
+    <footer className='flex justify-center p-3 mt-64 bg-[#D46253] opacity-80'>
       <p className='hidden lg:flex lg:absolute lg:left-10 font-medium text-sm text-[#f9f9f9]'>© 2023 Felipe Vahia Malliagros</p>
       <nav className='items-center space-x-4 md:space-x-48 uppercase text-[#f9f9f9] font-medium text-sm'>
         <NavOptionNavigation className='hover:opacity-80' componentId='content' areaNameNav='knowledge'>Knowledge</NavOptionNavigation>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -19,6 +19,40 @@ export const LandingPage: React.FC = () => {
           <img src="src/assets/background/circle.svg" className='absolute -top-[70%] left-[65%] 2xl:-top-[110%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img>
           {/* <img src="src/assets/background/circle.svg" className='absolute opacity-80 -top-[70%] left-[70%] 2xl:-top-[80%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img> */}
         </article>
+        <div className='absolute -z-10 bg-[#E48383] mt-48 w-screen h-[60rem]'/>
+        <article className='w-fill md:mx-16 2xl:w-[89.938rem] 2xl:m-auto'>
+          <section className='relative w-full mt-96 container'>
+            <aside className='flex justify-start'>
+              <div className='flex relative container w-[544px] right-0 h-[416px] rounded-tl-[200px] bg-[#fbfbfb] border border-[#E48383] text-[#2E3E4B]'>
+                <h2 className='absolute top-14 font-semibold text-4xl right-28'>
+                About
+                </h2>
+                <div className='absolute flex flex-col items-center justify-center p-14 left-[72px] top-36 w-[628px] h-[320px] bg-[#fbfbfb] border-2 border-[#E48383] rounded-[50px] text-lg'>
+                  <p>Me chamo Felipe Vahia Malliagros, 22 anos e moro no Rio de Janeiro, capital. Cursei metade da graduação de engenharia civil e resolvi mudar de carreira. Desde então, não parei de estudar programação. E é fácil, pois amo programar.
+                  </p>
+                  <p className='mt-2'>
+                    Fiz estágio por 4 meses na Trybe. Com Scrum, criei e aprimorei conteúdos, exercícios, aulas e projetos. Envolveram HTML, CSS, JavaScript (ES6), React (Context, Redux), testes com Jest e React Testing Library. 
+                  </p>
+                </div>
+              </div>
+            </aside>
+            <aside className='flex justify-end'>
+              <div className='flex relative container w-[544px] right-0 mt-12 h-[416px] rounded-tr-[200px] bg-[#fbfbfb] border border-[#E48383] text-[#2E3E4B]'>
+                <h2 className='absolute top-14 font-semibold text-4xl left-28'>
+                Academics
+                </h2>
+                <div className='absolute flex flex-col items-center justify-center p-14 right-[72px] top-36 w-[628px] h-[320px] bg-[#fbfbfb] border-2 border-[#E48383] rounded-[50px] text-lg'>
+                  <p>
+                  Graduando em Ciências da Computação na UFF e me aprimorando em cloud e microsserviços. Além dos meus estudos pessoais, tenho intenção de alinhar o caminho dos meus estudos com as necessidades da empresa que eu estiver.
+                  </p>
+                  <p className='mt-2'>
+                    Após muito tempo de estudo, já me sinto confiante para começar a trabalhar com qualquer tecnologia do mercado.
+                  </p>
+                </div>
+              </div>
+            </aside>
+          </section>
+        </article>
         <Footer />
       </main>
     </div>
