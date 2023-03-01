93410303f98b138f94283e89c6d4ff743619bc5e
felipemalli <felipevahia@gmail.com>
2023-03-01 07:03:49 -0300

    feat: update portfolio project data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,12 +5,16 @@ export const projects: ICard[] = [
     name: 'Portfolio',
     type: 'project',
     creator: 'Felipe Vahia Malliagros',
-    description: 'Project developed to expose my knowledge. Projects, courses taken, skills already trained, my future steps and goals. OBS: I will develop a backend for this application soon!',
-    inProgress: true,
+    description: 'Project developed to expose my knowledge. I started by designing the design in figma, and then applied it to the front-end. While developing, I decided to change some details. OBS: I will develop a backend for this application soon! And it will be easy, because I developed this frontend project already receiving structured and scalable data. So, I\'ll take the advantage of this to train something new on the backend/devops.',
+    inProgress: false,
     image: 'src/assets/projects/portfolio.webp',
     techAreas: {
       frontend: {
-        skills: ['Typescript', 'React', 'Vite', 'Tailwind', 'Figma'],
+        skills: ['Typescript', 'React', 'Vite', 'Tailwind', 'Figma', 'CSS'],
+      },
+      backend: {
+        skills: [''],
+        inProgress: true,
       }
     },
     links: [
