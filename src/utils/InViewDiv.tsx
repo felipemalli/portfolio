339dfc21ec6da58b3bc4e1d67a5acd2ef338ebe2
felipemalli <felipevahia@gmail.com>
2023-03-02 06:34:35 -0300

    feat: change motion in inviewdiv utils to use observer

--- src/utils/InViewDiv.tsx
+++ src/utils/InViewDiv.tsx
@@ -1,22 +1,47 @@
-import { useInView } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IChildrenProps } from '../interfaces';
-interface IInviewProps extends IChildrenProps {
-  translation?: string,
-  duration?: string,
-  delay?: string,
+
+interface IInViewProps extends IChildrenProps {
+  translation?: string;
+  duration?: string;
+  delay?: string;
 }
 
-export const InViewDiv: React.FC<IInviewProps> = ({ children, translation= 'translateX(400px)', duration= '0.9s', delay= '0.5s' }: IInviewProps ) => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+export const InViewDiv: React.FC<IInViewProps> = ({ children, translation = 'translateX(400px)', duration = '0.9s', delay = '0.5s'}: IInViewProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isInView, setIsInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsInView(true);
+            if (ref.current) {
+              observer.unobserve(ref.current);
+            }
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <div ref={ref} style={{
-      opacity: isInView ? 1 : 0,
-      transform: isInView ? 'none' : `${translation}`,
-      transition: `all ${duration} ${delay}`
-    }}>
+    <div
+      ref={ref}
+      style={{
+        opacity: isInView ? 1 : 0,
+        transform: isInView ? 'none' : `${translation}`,
+        transition: `all ${duration} ${delay}`,
+      }}
+    >
       {children}
     </div>
   );
