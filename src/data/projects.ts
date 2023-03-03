c59b02797c052cea56a5fbca087a841e408c6dfa
felipemalli <felipevahia@gmail.com>
2023-03-03 01:47:53 -0300

    feat: add data of a lot of projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -123,6 +123,36 @@ export const projects: ICard[] = [
     ],
   },
   {
+    name: 'Car Shop',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'API with a simple CRUD for handling cars. I follow the concepts of SOLID, OOP and using generics for reuse.',
+    inProgress: false,
+    image: 'src/assets/projects/carshop.webp',
+    techAreas: {
+      backend: {
+        skills: ['Typescript', 'Node', 'MongoDB', 'Mongoose', 'Jest', 'Mocha/Chai/Sinon', 'OOP', 'SOLID'],
+      },
+      devops: {
+        skills: ['Docker'],
+      }
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/car-shop'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+  },
+  {
     name: 'Miles Program',
     type: 'project',
     creator: 'Felipe Vahia Malliagros',
@@ -138,6 +168,33 @@ export const projects: ICard[] = [
       {
         name: 'Code',
         link: 'https://github.com/felipemalli/miles-program'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+  },
+  {
+    name: 'Recipes App',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'Front-End project of a recipes app made in team. It was used in the development of the project, two databases of 2 different APIs, one for food and the other for drinks. We divided the tasks so as not to overload anyone on the team, and we were able to deliver the project in advance.',
+    inProgress: false,
+    image: 'src/assets/projects/recipesapp.webp',
+    techAreas: {
+      frontend: {
+        skills: ['Javascript', 'React', 'Styled Components'],
+      },
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/project-recipes-app'
       },
       {
         name: 'Browser',
@@ -173,6 +230,153 @@ export const projects: ICard[] = [
       {
         name: 'API',
         link: 'https://github.com/felipemalli/register-specialties#operations'
+      },
+    ],
+  },
+  {
+    name: 'Blogs App',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'API REST with various CRUD endpoints for handling blogs posts. One observation is that Joi was used for middleware validations.',
+    inProgress: false,
+    image: 'src/assets/projects/blogsapp.webp',
+    techAreas: {
+      backend: {
+        skills: ['Javascript', 'Node', 'Express', 'Sequelize', 'MySQL', 'JWT'],
+      }
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/blogs-app'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: 'https://github.com/felipemalli/blogs-app#endpoints'
+      },
+    ],
+  },
+  {
+    name: 'Wallet App',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'Front-end wallet app that I developed to improve my skills with redux React Redux',
+    inProgress: false,
+    image: 'src/assets/projects/walletapp.webp',
+    techAreas: {
+      frontend: {
+        skills: ['Javascript', 'React', 'Redux'],
+      },
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/wallet-app'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+  },
+  {
+    name: 'Clean Schedule',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'I\'m doing this API to train Clean Arquitecture, TDD, DDD and for consolidate more SOLID concepts.',
+    inProgress: true,
+    image: 'src/assets/projects/cleanschedule.webp',
+    techAreas: {
+      backend: {
+        skills: ['Typescript', 'Node', 'Clean Architecture', 'TDD', 'DDD', 'Design Patterns', 'Jest', 'GraphQL', 'MongoDB', 'Mongoose', 'Swagger', 'SOLID', 'OOP', 'Husky', 'CI/CD', 'BCrypt']
+      },
+      devops: {
+        skills: ['Docker', 'CI/CD'],
+      }
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/clean_schedule_api'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+  },
+  {
+    name: 'To do list',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'To do list for train material UI. The backend was made with Node, Express, Sequelize and MySQL, but I\'m going to create another one with other technologies. But you can access the old one in my github.',
+    inProgress: true,
+    image: 'src/assets/projects/todolist.webp',
+    techAreas: {
+      frontend: {
+        skills: ['Typescript', 'React', 'Material UI'],
+      },
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/to-do-list-ui'
+      },
+      {
+        name: 'Browser',
+        link: 'https://sunny-frangollo-bd1940.netlify.app'
+      },
+      {
+        name: 'API',
+        link: ''
+      },
+    ],
+    bottonRightTag: {
+      name: 'Browser',
+    }
+  },
+  {
+    name: 'Musicall',
+    type: 'project',
+    creator: 'Felipe Vahia Malliagros',
+    description: 'A full-stack project of a a web music page. I\'m learning a lot with Nest, Prisma and Next doing this project',
+    inProgress: true,
+    image: 'src/assets/projects/musicall.webp',
+    techAreas: {
+      backend: {
+        skills: ['Typescript', 'Node', 'Nest', 'Prisma', 'OOP'],
+      },
+      frontend: {
+        skills: ['Typescript', 'Next']
+      },
+      devops: {
+        skills: ['Docker'],
+      }
+    },
+    links: [
+      {
+        name: 'Code',
+        link: 'https://github.com/felipemalli/musicall'
+      },
+      {
+        name: 'Browser',
+        link: ''
+      },
+      {
+        name: 'API',
+        link: ''
       },
     ],
   },
