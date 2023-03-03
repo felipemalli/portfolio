494672778eef2f403a88337c3a7f5a1d7bb22c64
felipemalli <felipevahia@gmail.com>
2023-03-03 06:36:04 -0300

    fix: circle image stop scrolling x

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,7 +8,7 @@ import { AnimatedDiv } from '../utils/AnimatedDiv';
 
 export const LandingPage: React.FC = () => {
   return (
-    <main>
+    <main className='w-screen'>
       <article className='w-fill md:mx-16 2xl:w-[89.938rem] 2xl:m-auto'>
         <AnimatedDiv initialAnimation='-translate-y-24' finalAnimation='translate-y-0' duration='duration-1800'>
           <Header />
@@ -18,7 +18,11 @@ export const LandingPage: React.FC = () => {
           <IntroductionSection />
           <ContentCardSection />
         </AnimatedDiv>
-        <img src={backgroundImages.circle} className='absolute -top-[70%] left-[65%] 2xl:-top-[110%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img>
+        <div className='absolute top-0 right-0 h-[200rem] w-screen -z-10'>
+          <div className="relative w-100% h-[60vw] overflow-hidden -z-10">
+            <img src={backgroundImages.circle} className='absolute -right-[65%] -top-[85%] -z-10 scale-125 invisible xl:visible' alt='Background circle'></img>
+          </div>
+        </div>
       </article>
       <div className='absolute bg-primary-300 mt-48 w-screen h-[70rem] sm:h-[65rem] md:h-[71rem] 2xl:h-[60rem] -z-10'/>
       <article className='md:mx-6 lg:mx-16 2xl:w-[89.938rem] 2xl:m-auto'>
