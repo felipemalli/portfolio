bd8d0eb923a9c3099d5cbfaadd2c603b4f7f8a28
felipemalli <felipevahia@gmail.com>
2023-03-04 20:51:06 -0300

    feat: use translation into header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -22,7 +22,7 @@ export const Header: React.FC = () => {
     }
   };
   
-  const { language, changeLanguage } = useLanguageContext();
+  const { translations, language, changeLanguage } = useLanguageContext();
   let languageName: string;
 
   const updateLanguage = () => {
@@ -37,20 +37,20 @@ export const Header: React.FC = () => {
       <div className='flex'>
         <div className={`${!isOpen ? 'hidden' : 'absolute'} w-screen md:w-auto md:flex md:relative right-1/2 translate-x-1/2 mt-12 md:mt-0 border-y md:border-none`}>
           <nav ref={navRef} className={'flex flex-col md:flex-row items-center text-center py-4 md:py-0 space-y-2 md:space-y-0 md:space-x-6 uppercase text-customBlue-500 tracking-widest font-semibold bg-secondary-900 md:bg-transparent'} onClick={() => setIsOpen(false)}>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='knowledge'>Knowledge</NavOptionNavigation>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='skills'>Skills</NavOptionNavigation>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='about'>About</NavOptionNavigation>
-            <NavOptionLink className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>Contact</NavOptionLink>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='knowledge'>{translations.nav_knowledge}</NavOptionNavigation>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='skills'>{translations.nav_skills}</NavOptionNavigation>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='about'>{translations.nav_about}</NavOptionNavigation>
+            <NavOptionLink className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>{translations.nav_contact}</NavOptionLink>
           </nav>
         </div>
-        <div className='absolute right-0 top-5'>
-          <button className='flex h-10 w-36 scale-75 md:-mr-16 mt-2 space-x-2 border rounded-lg border-black items-center justify-evenly'
+        <div className='absolute right-0 2xl:-right-24 top-4'>
+          <button className='flex h-10 w-44 scale-75 md:-mr-16 mt-2 space-x-2 bg-secondary-900 rounded border-[1px] border-black items-center justify-evenly'
             onClick={ () => {
               language === 'pt-br' && changeLanguage('en');
               language === 'en' && changeLanguage('pt-br');
               updateLanguage();
             }}>
-            <p className='text-lg'>{languageName}</p>
+            <p className='text-xl text-customBlue-500 font-medium'>{languageName}</p>
             <img className='flex h-6 w-6' src={iconImages.languageGlobe} alt='Language icon'/>
           </button>
         </div>
