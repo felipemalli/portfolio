2f0930d5786dc29fb2d80cac1df12c6847706404
felipemalli <felipevahia@gmail.com>
2023-03-04 19:17:31 -0300

    feat: implement language context

---
+++ src/contexts/ LanguageContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import { IChildrenProps } from '../interfaces';
+
+type ILanguageOptions = 'pt-br' | 'en';
+
+interface ITranslation {
+  [key: string]: string;
+}
+
+interface ILanguageContextData {
+  language: string;
+  changeLanguage: (lang: ILanguageOptions) => void;
+  translations: ITranslation;
+}
+
+export const LanguageContext = createContext<ILanguageContextData>({} as ILanguageContextData);
+
+export const useLanguageContext = () => {
+  return useContext(LanguageContext);
+};
+
+const LanguageProvider: React.FC<IChildrenProps> = ({ children }: IChildrenProps) => {
+  const [language, setLanguage] = useState<ILanguageOptions>('pt-br');
+  const [translations, setTranslations] = useState<ITranslation>({});
+
+  useEffect(() => {
+    const loadTranslations = async () => {
+      const res = await fetch(`../data/languages/${language}.json`);
+      const json = await res.json();
+      setTranslations(json);
+    };
+    loadTranslations();
+  }, [language]);
+
+  const changeLanguage = useCallback((language: ILanguageOptions) => {
+    setLanguage(language);
+  }, [setLanguage]);
+
+  return (
+    <LanguageContext.Provider 
+      value={
+        { 
+          language,
+          changeLanguage,
+          translations,  
+        }
+      }
+    >
+      { children }
+    </LanguageContext.Provider>
+  );
+};
+
+export default LanguageProvider;
