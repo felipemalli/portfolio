7f239d120129bf6f03e83cdfe52600458df15f9f
felipemalli <felipevahia@gmail.com>
2023-03-04 05:20:37 -0300

    style: add background div for smaller devices

--- src/components/about-section/AboutSection.tsx
+++ src/components/about-section/AboutSection.tsx
@@ -28,8 +28,8 @@ export const AboutSection: React.FC = () => {
               </p>
             </div>
           </div>
-          {/* <div className="absolute h-1/2 -bottom-1/2 right-0 w-screen bg-primary-300 -z-20" /> */}
         </InViewDiv>
+        <div className="absolute h-1/2 -bottom-1/2 right-0 w-screen bg-primary-300 -z-20" />
       </aside>
       <aside className='flex relative justify-center md:justify-end items-end mt-36 md:mt-52 2xl:mt-6 md:px-6 lg:px-16 2xl:px-[12vw]'>
         <InViewDiv delay='1s'>
