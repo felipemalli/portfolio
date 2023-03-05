bd9689c09e0cf7d25814e9b6ba81c5fa9689ba39
felipemalli <felipevahia@gmail.com>
2023-03-05 20:49:25 -0300

    feat: create media query context

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import { ChangeAreaProvider, CourseProvider, LanguageProvider, ProjectProvider, SearchProvider, SkillProvider } from './contexts';
+import { ChangeAreaProvider, CourseProvider, LanguageProvider, MediaQueryProvider, ProjectProvider, SearchProvider, SkillProvider } from './contexts';
 import { LandingPage } from './pages/LandingPage';
 
 function App() {
   return (
-    <LanguageProvider>
-      <SearchProvider>
-        <ChangeAreaProvider>
-          <ProjectProvider>
-            <CourseProvider>
-              <SkillProvider>
-                <LandingPage/>
-              </SkillProvider>
-            </CourseProvider>
-          </ProjectProvider>
-        </ChangeAreaProvider>
-      </SearchProvider>
-    </LanguageProvider>
+    <MediaQueryProvider>
+      <LanguageProvider>
+        <SearchProvider>
+          <ChangeAreaProvider>
+            <ProjectProvider>
+              <CourseProvider>
+                <SkillProvider>
+                  <LandingPage/>
+                </SkillProvider>
+              </CourseProvider>
+            </ProjectProvider>
+          </ChangeAreaProvider>
+        </SearchProvider>
+      </LanguageProvider>
+    </MediaQueryProvider>
   );
 }
 
---
+++ src/contexts/MediaQueryContext.tsx
@@ -0,0 +1,51 @@
+import React, { createContext, useContext } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { IChildrenProps } from '../interfaces';
+import { breakpoints } from '../utils/breakpoints';
+
+type MediaQuery = {
+  isSm: boolean;
+  isMd: boolean;
+  isLg: boolean;
+  isXl: boolean;
+  is2xl: boolean;
+  is3xl: boolean;
+};
+
+const MediaQueryContext = createContext<MediaQuery>({
+  isSm: false,
+  isMd: false,
+  isLg: false,
+  isXl: false,
+  is2xl: false,
+  is3xl: false,
+} as MediaQuery);
+
+export const useMediaQueryContext = () => {
+  return useContext(MediaQueryContext);
+};
+
+export const MediaQueryProvider: React.FC<IChildrenProps> = ({ children }: IChildrenProps) => {
+  const isSm = useMediaQuery(breakpoints.isSm);
+  const isMd = useMediaQuery(breakpoints.isMd);
+  const isLg = useMediaQuery(breakpoints.isLg);
+  const isXl = useMediaQuery(breakpoints.isXl);
+  const is2xl = useMediaQuery(breakpoints.is2xl);
+  const is3xl = useMediaQuery(breakpoints.is3xl);
+
+  return (
+    <MediaQueryContext.Provider 
+      value={
+        {
+          isSm,
+          isMd,
+          isLg,
+          isXl,
+          is2xl,
+          is3xl,
+        }
+      }>
+      { children }
+    </MediaQueryContext.Provider>
+  );
+};
