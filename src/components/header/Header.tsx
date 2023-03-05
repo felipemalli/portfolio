b159daed095a701087b08194fb38a2fa46a175d2
felipemalli <felipevahia@gmail.com>
2023-03-04 22:40:05 -0300

    style: change margins of header and introduction section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -33,26 +33,28 @@ export const Header: React.FC = () => {
 
   return (
     <header className='flex justify-between md:justify-center -mb-[88px] xl:justify-start p-6 md:my-4 xl:ml-20 2xl:ml-12'>
-      <img className='flex h-6 w-6 scale-75 md:-ml-16 md:mr-16 mt-2' src={backgroundImages.icon} alt='Felipevm icon'/>
+      <img className='flex h-6 w-6 scale-75 xl:-ml-28 2xl:-ml-16 mr-10 2xl:mr-16 mt-2' src={backgroundImages.icon} alt='Felipevm icon'/>
       <div className='flex'>
         <div className={`${!isOpen ? 'hidden' : 'absolute'} w-screen md:w-auto md:flex md:relative right-1/2 translate-x-1/2 mt-12 md:mt-0 border-y md:border-none`}>
-          <nav ref={navRef} className={'flex flex-col md:flex-row items-center text-center py-4 md:py-0 space-y-2 md:space-y-0 md:space-x-6 uppercase text-customBlue-500 tracking-widest font-semibold bg-secondary-900 md:bg-transparent'} onClick={() => setIsOpen(false)}>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='knowledge'>{translations.nav_knowledge}</NavOptionNavigation>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='skills'>{translations.nav_skills}</NavOptionNavigation>
-            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' componentId='about'>{translations.nav_about}</NavOptionNavigation>
-            <NavOptionLink className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent px-2 w-screen h-10 md:w-auto flex items-center justify-center' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>{translations.nav_contact}</NavOptionLink>
+          <nav ref={navRef} className={'flex flex-col md:flex-row items-center text-center py-4 md:py-0 space-y-2 md:space-y-0 space-x-8 2xl:space-x-10 uppercase text-customBlue-500 tracking-widest font-semibold bg-secondary-900 md:bg-transparent'} onClick={() => setIsOpen(false)}>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent  w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='knowledge'>{translations.nav_knowledge}</NavOptionNavigation>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent  w-screen h-10 md:w-auto flex items-center justify-center' componentId='content' areaNameNav='skills'>{translations.nav_skills}</NavOptionNavigation>
+            <NavOptionNavigation className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent  w-screen h-10 md:w-auto flex items-center justify-center' componentId='about'>{translations.nav_about}</NavOptionNavigation>
+            <NavOptionLink className='hover:scale-105 hover:bg-customGray-200 md:hover:bg-transparent  w-screen h-10 md:w-auto flex items-center justify-center' url='https://www.linkedin.com/in/felipe-vahia-malliagros/'>{translations.nav_contact}</NavOptionLink>
           </nav>
         </div>
-        <div className='absolute right-0 2xl:-right-24 top-4'>
-          <button className='flex h-10 w-44 scale-75 md:-mr-16 mt-2 space-x-2 bg-secondary-900 rounded border-[1px] border-black items-center justify-evenly'
+        <div className='absolute right-16 md:-right-24 top-4'>
+          <button className='flex h-10 w-12 scale-75 md:-mr-16 mt-2 space-x-2 items-center justify-evenly'
             onClick={ () => {
               language === 'pt-br' && changeLanguage('en');
               language === 'en' && changeLanguage('pt-br');
               updateLanguage();
             }}>
-            <p className='text-xl text-customBlue-500 font-medium'>{languageName}</p>
             <img className='flex h-6 w-6' src={iconImages.languageGlobe} alt='Language icon'/>
           </button>
+          <div className=' bg-secondary-900 '>
+
+          </div>
         </div>
         <button ref={navButton} type='button' className='h-10 w-10 block md:hidden  focus:outline-none hover:bg-customGray-200 rounded-full pl-2' onClick={() => setIsOpen(!isOpen)}>
           {isOpen 
--- src/components/introduction-section/IntroductionSection.tsx
+++ src/components/introduction-section/IntroductionSection.tsx
@@ -8,7 +8,7 @@ export const IntroductionSection: React.FC = () => {
   const { translations, language } = useLanguageContext();
 
   return (
-    <section className='flex flex-col xl:flex-row justify-center 2xl:justify-evenly items-center 2xl:gap-32 gap-3 2xl:mt-0 h-screen md:h-auto'>
+    <section className='flex flex-col xl:flex-row justify-evenly items-center 2xl:gap-32 gap-3 -mt-8 xl:-mt-10 2xl:mt-0 h-screen md:h-auto'>
       <div className='flex flex-col justify-center lg:w-[32rem] xl:-mb-12 2xl:mb-0 mt-10'>
         <h1 className='text-customBlue-900 text-6xl font-semibold text-center xl:text-left'>Felipe Malliagros</h1>
         <div className='text-customBlue-500 text-2xl font-semibold mt-7 text-center xl:text-left'>
@@ -40,7 +40,7 @@ export const IntroductionSection: React.FC = () => {
           </button>
         </div>
       </div>
-      <div className='md:flex items-center justify-center mt-10 xl:mt-0 hidden'>
+      <div className='md:flex items-center justify-center mt-10 md:mt-4 2x:mt-10 xl:mt-0 hidden mr-0 xl:mr-6 2xl:mr-0'>
         {backgroundImages.felipeEmpty && 
           <AnimatedDiv initialAnimation='translate-y-150' finalAnimation='translate-y-0' duration='duration-1400' delay='delay-750'>
             <div className='w-[30rem] lg:w-[33rem] 2xl:w-[40rem] mt-10 h-[408.19px] lg:h-[449px] 2xl:h-[544.25px]'>
