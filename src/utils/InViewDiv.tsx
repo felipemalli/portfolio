6da0cf340c25e7bf4ac9638ceca84ed62df27938
felipemalli <felipevahia@gmail.com>
2023-03-06 20:24:53 -0300

    style: remove inview delay

--- src/utils/InViewDiv.tsx
+++ src/utils/InViewDiv.tsx
@@ -23,7 +23,7 @@ export const InViewDiv: React.FC<IInViewProps> = ({ children, translation = 'tra
           }
         });
       },
-      { threshold: 0.20 }
+      { threshold: 0.30 }
     );
     if (ref.current) {
       observer.observe(ref.current);
