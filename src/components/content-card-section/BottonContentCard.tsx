af4bc441dab37cdb66693541adab847d3c12f2a0
felipemalli <felipevahia@gmail.com>
2023-03-06 02:26:27 -0300

    feat: botton and top now update and re-render cards when language changes

--- src/components/content-card-section/BottonContentCard.tsx
+++ src/components/content-card-section/BottonContentCard.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { useCourseContext, useLanguageContext, useSkillContext } from '../../contexts';
 import { useChangeAreaContext } from '../../contexts/ChangeAreaContext';
-import { enCourses } from '../../data';
+import { enCourses, ptBrCourses } from '../../data';
 import { bottonSkills } from '../../data/universal/skills';
+import { ICard } from '../../interfaces';
 import { CARD_SIZE } from '../shared/Card/Card';
 import { SliderFiveColumns } from '../shared/Slider/SliderFiveColumns';
 import { ContentArea } from './particular/ContentArea';
@@ -11,14 +13,30 @@ export const BottonContentCard: React.FC = () => {
   const { areaName } = useChangeAreaContext();
   const { setFilteredBottonSkills, setAllBottonSkills, filteredBottonSkills } = useSkillContext();
 
-  const { translations } = useLanguageContext();
+  const { translations, language } = useLanguageContext();
+  const { filteredCourses, setFilteredCourses, setAllCourses } = useCourseContext();
 
-  const { setAllCourses, filteredCourses, setFilteredCourses } = useCourseContext();
+  const [courses, setCourses] = useState<ICard[]>([]);
+
+  const handleData = () => {
+    if (language === 'pt-br') {
+      setCourses(ptBrCourses);
+    } else {
+      setCourses(enCourses);
+    }
+    setFilteredCourses(courses);
+  };
+
+  useEffect(() => {
+    handleData();
+  }, [translations, language]);
   
   return (
     <div className='flex flex-col justify-evenly items-center h-[22rem] xl:h-[24rem] 2xl:h-[26rem] m-auto md:rounded-b-[5rem] border-y md:border border-customBlue-300 bg-secondary-500 pt-1'>
       <SliderFiveColumns CARD_SIZE={CARD_SIZE}>
-        <ContentArea data={enCourses} filteredData={filteredCourses} setFiltered={setFilteredCourses} setAll={setAllCourses} notFoundTranslation={translations.courses_not_found} />
+        {courses.length > 0 && (
+          <ContentArea data={courses} filteredData={filteredCourses} setFiltered={setFilteredCourses} setAll={setAllCourses} notFoundTranslation={translations.courses_not_found} />
+        )}
         <SkillArea type={'botton'} skills={bottonSkills} setFilteredSkills={setFilteredBottonSkills} setAllSkills={setAllBottonSkills} filteredSkills={filteredBottonSkills} />
       </SliderFiveColumns>
       <h2 className={`text-customBlue-900 select-none -mt-7 pt-3 md:pt-0 md:-mt-3 h-10 font-semibold ${areaName === 'knowledge' ? 'text-2xl md:text-4xl ' : 'text-[1.3rem] md:text-[2rem]'}`}>
--- src/components/content-card-section/TopContentCard.tsx
+++ src/components/content-card-section/TopContentCard.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import { useLanguageContext, useProjectContext, useSkillContext } from '../../contexts';
 import { useChangeAreaContext } from '../../contexts/ChangeAreaContext';
-import { enProjects } from '../../data';
+import { enProjects, ptBrProjects } from '../../data';
 import { topSkills } from '../../data/universal/skills';
+import { ICard } from '../../interfaces';
 import { CARD_SIZE } from '../shared/Card/Card';
 import { SliderFiveColumns } from '../shared/Slider/SliderFiveColumns';
 import { ContentArea } from './particular/ContentArea';
@@ -11,9 +13,23 @@ export const TopContentCard: React.FC = () => {
   const { areaName } = useChangeAreaContext();
   const { setFilteredTopSkills, setAllTopSkills, filteredTopSkills } = useSkillContext();
 
-  const { translations } = useLanguageContext();
-
+  const { translations, language } = useLanguageContext();
   const { filteredProjects, setFilteredProjects, setAllProjects } = useProjectContext();
+
+  const [projects, setProjects] = useState<ICard[]>([]);
+
+  const handleData = () => {
+    if (language === 'pt-br') {
+      setProjects(ptBrProjects);
+    } else {
+      setProjects(enProjects);
+    }
+    setFilteredProjects(projects);
+  };
+
+  useEffect(() => {
+    handleData();
+  }, [translations, language]);
 
   return (
     <div className='flex flex-col justify-evenly items-center h-[22rem] xl:h-[24rem] 2xl:h-[26rem] m-auto md:rounded-t-[5rem] shadow-inner bg-primary-500'>
