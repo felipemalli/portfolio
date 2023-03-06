c0f52666186c950bd20a4224f9fec00dd25a12d3
felipemalli <felipevahia@gmail.com>
2023-03-06 17:46:13 -0300

    style: change space between options at footer by breakpoints

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,8 +8,8 @@ export const Footer: React.FC = () => {
       <div className='absolute -top-5 left-1/2 -translate-x-1/2 z-10'>
         <NavOptionNavigation componentId='header' className='hover:opacity-80' ><img src='./assets/icons/upArrow.svg' /></NavOptionNavigation>
       </div>
-      <p className='hidden 2xl:flex 2xl:absolute lg:left-10 font-medium text-sm text-secondary-700'>© 2023 Felipe Vahia Malliagros</p>
-      <nav className='items-center space-x-36 lg:space-x-48 text-secondary-700 font-medium sm:text-sm text-xs'>
+      <p className='hidden lg:flex lg:absolute lg:left-10 font-medium text-sm text-secondary-700'>© 2023 Felipe Vahia Malliagros</p>
+      <nav className='items-center space-x-28 md:space-x-36 lg:space-x-28 xl:space-x-48 text-secondary-700 font-medium sm:text-sm text-xs'>
         <NavOptionLink className='hover:opacity-80' url='https://github.com/felipemalli'>Github</NavOptionLink>
         <NavOptionLink className='hover:opacity-80' url='https://www.linkedin.com/in/felipe-vahia-malliagros'>Linkedin</NavOptionLink>
       </nav>
