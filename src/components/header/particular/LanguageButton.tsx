b4282e5d806b3408d3874e434c7e93568ff05d88
felipemalli <felipevahia@gmail.com>
2023-03-06 20:46:01 -0300

    style: make language button have a background when hovering

--- src/components/header/particular/LanguageButton.tsx
+++ src/components/header/particular/LanguageButton.tsx
@@ -38,11 +38,11 @@ export const LanguageButton: React.FC<ILanguageButtonProps> = ({ className, hasT
   }, [language]);
   
   return (
-    <div ref={languageRef} className={`${className} md:hover:scale-95 scale-90`}>
-      <button className={`flex h-10 items-center ${hasText ? 'justify-end' : 'justify-center'} space-x-[5px] xl:w-full w-10 h-10 hover:bg-customGray-200 md:hover:bg-secondary-500 rounded-full -ml-2 xl:ml-0`}
+    <div ref={languageRef} className={`${className} scale-90`}>
+      <button className={`flex h-10 items-center ${hasText ? 'justify-end' : 'justify-center'} space-x-[5px] xl:w-full w-10 h-10 hover:bg-customGray-200 xl:p-2 rounded-full -ml-2 xl:ml-0`}
         onClick={ () => setIsOpenLanguage(!isOpenLanguage)}>
         {hasText && <p className='font-semibold text-customBlue-500 uppercase text-sm -mb-1 tracking-widest'>{translations.language}</p>}
-        <img className='flex h-6 w-6' src='./assets/icons/languageGlobe.svg' alt='Language icon'/>
+        <img className='flex h-6 w-6 hover:text-customBlue-300' src='./assets/icons/languageGlobe.svg' alt='Language icon'/>
       </button>
       <div className='absolute top-10 left-1/2 -translate-x-1/2 flex flex-col gap-1'>
         {isOpenLanguage && Object.entries(languageNames).map(([languageShort, languageName]) => (
