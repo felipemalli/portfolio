adf51f2e00f5651aed036c6794f69a7d524eb575
felipemalli <felipevahia@gmail.com>
2023-03-05 22:52:35 -0300

    fix: reduce footer texts for prevent breaking very small devices

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className='flex justify-center p-3 mt-40 md:mt-48 bg-primary-500'>
       <p className='hidden 2xl:flex 2xl:absolute lg:left-10 font-medium text-sm text-secondary-700'>© 2023 Felipe Vahia Malliagros</p>
-      <nav className='items-center space-x-5 md:space-x-20 lg:space-x-48 uppercase text-secondary-700 font-medium text-sm'>
+      <nav className='items-center space-x-5 md:space-x-20 lg:space-x-48 uppercase text-secondary-700 font-medium sm:text-sm text-xs'>
         <NavOptionNavigation className='hover:opacity-80' componentId='content' areaNameNav='knowledge'>{translations.nav_knowledge}</NavOptionNavigation>
         <NavOptionNavigation className='hover:opacity-80' componentId='content' areaNameNav='skills'>{translations.nav_skills}</NavOptionNavigation>
         <NavOptionNavigation className='hover:opacity-80' componentId='about'>{translations.nav_about}</NavOptionNavigation>
